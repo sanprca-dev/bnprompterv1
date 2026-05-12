bb45c661455733cf0b0e728bf63115f5cd9ca36d
sanprca-dev <san.prca@gmail.com>
2026-05-12 09:01:01 -0500

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-  API_URL: 'https://script.google.com/macros/s/AKfycbyNmXp5Le4gPupIxc-51jofEMWr5JEm2iBSTXQ_u3DH1GmkJEiippTsGWfoOjruCjRYFw/exec?doc=1thbg6iA5z-eXFnVaVhxB4zxFv3FovAMNwvx77hXrk-k',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxv3ZhHBo4NlANd8zOo9MQBhHIL4BhM1GMQfhtmyEEBD74SN9h4snNJCsABMeq98OgyTw/exec?doc=1thbg6iA5z-eXFnVaVhxB4zxFv3FovAMNwvx77hXrk-k',
 
   REFRESH_INTERVAL: 5000,
 
