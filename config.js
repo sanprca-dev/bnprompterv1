340350d55dc334a23962f48f882155a2e3d7c510
sanprca-dev <san.prca@gmail.com>
2026-05-12 13:10:46 -0500

    Update API_URL in config.js
    se actualiza api_url por error en cargado de texto en el prompter

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-  API_URL: 'https://script.google.com/macros/s/AKfycbzhYooNojqMyzaFKiFlejQjNM-aHFLASjFytPDyZWx-UpwnigC8QSwb9YDI_wu-1q8hag/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyc9ec1hiRY6pVwLz2xXT66SQ3Pj0E4POT5yLSdHRi59gQe140zUGP_4lc5ShoMFV9tBw/exec',
 
   REFRESH_INTERVAL: 5000,
 
