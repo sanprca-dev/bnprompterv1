a3c6966703ba72e905c36cbe9c9b722342eda529
sanprca-dev <san.prca@gmail.com>
2026-05-12 13:29:17 -0500

    Update API_URL in config.js
    Se actualiza API_URL. Faltaba permisos de la app para jalar el CSS de el googledoc

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-  API_URL: 'https://script.google.com/macros/s/AKfycbyc9ec1hiRY6pVwLz2xXT66SQ3Pj0E4POT5yLSdHRi59gQe140zUGP_4lc5ShoMFV9tBw/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwjwVPalyOO-jewT77SJEDaNYFhMR0tmfkYp2GJoG8gtbQGjW7nO2N2PR2lQqguegFYIw/exec',
 
   REFRESH_INTERVAL: 5000,
 
